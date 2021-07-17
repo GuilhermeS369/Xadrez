ec2b5551f965cd7e2d7b52f1c78571dd1229cd79
GuilhermeS369 <guilherme460@yahoo.com.br>
2021-07-17 15:19:47 -0300

    Implementação da peça e do tabuleiro
    Iniciando com a implementação do tabuleiro e da peça do Xadrez.

---
+++ src/boardgame/Board.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Board {
+
+	private int rows; //LINHAS
+	private int columns; //COLUNAS
+	private Piece [][] pieces; //A MATRIZ 
+	
+	public Board(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns]; //A PEÇA VAI SER INTANCIADA NA QUANTIRDADE DE LINHAS E COLUNAS INFORMADAS
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+	
+}
